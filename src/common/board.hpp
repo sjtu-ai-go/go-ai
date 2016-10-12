098a84b6fc97cc04f3c30691cdf3e98bdc9b3e42
Vic Luo <vicluo96@gmail.com>
2016-10-12 19:06:44 +0800

    Implement GroupNode

--- src/common/board.hpp
+++ src/common/board.hpp
@@ -4,11 +4,13 @@
 #include <cstddef>
 #include <bitset>
 #include <climits>
+#include <cstdint>
 #include <cstring>
 #include <iterator>
 #include <functional>
 namespace board
 {
+    // GridPoint, {Black, White, NA}[W][H]
     template<std::size_t W, std::size_t H>
     struct GridPoint
     {
@@ -125,6 +127,55 @@ namespace std
             return h(bg.buf);
         }
     };
+}
+
+namespace board
+{
+    // GroupNode is a linked list storing information about one connected group
+    static_assert(CHAR_BIT == 8, "WTF! 1 char != 8 bits on your platform");
+    struct GroupNode;
+    static_assert(sizeof(GroupNode*) <= 8, "Pointer on your platform > 8. Compression unsupported");
+    struct alignas(8) GroupNode
+    {
+    private:
+        std::uintptr_t next: 48; // on AMD64 virtual address only supports low 48bits
+        Player player: 1;
+        unsigned int liberty: 15;
+    public:
+        GroupNode() = default;
+        GroupNode(GroupNode *pgn, Player p, unsigned int liberty):
+        next(reinterpret_cast<std::uintptr_t >(pgn)), player(p), liberty(liberty)
+        {}
+        GroupNode* getNext() const
+        {
+            return reinterpret_cast<GroupNode*>(next);
+        }
+        void setNext(GroupNode *pgn)
+        {
+            next = reinterpret_cast<std::uintptr_t>(pgn);
+        }
+        unsigned int getLiberty()
+        {
+            return liberty;
+        }
+        void setLiberty(unsigned int lib)
+        {
+            liberty = lib;
+        }
+        void addLiberty(int libDelta)
+        {
+            libDelta += libDelta;
+        }
+        Player getPlayer() const
+        {
+            return player;
+        }
+        void setPlayer(Player p)
+        {
+            player = p;
+        }
+    };
+    static_assert(sizeof(GroupNode) == 8, "Compression failed");
 }
 
 #endif
