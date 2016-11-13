1791e2669246c4d3e55aa709bf58b6886839b900
Vic Luo <vicluo96@gmail.com>
2016-11-13 16:24:53 +0800

    Print log to stderr

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -4,5 +4,5 @@
 #include <memory>
 #include "logger.hpp"
 
-std::shared_ptr<spdlog::logger> LoggerSingleton::logger = spdlog::stdout_color_mt("default");
+std::shared_ptr<spdlog::logger> LoggerSingleton::logger = spdlog::stderr_color_mt("default");
 
