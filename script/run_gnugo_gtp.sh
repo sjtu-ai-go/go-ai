76109512f3f8ef0da82090eee89f5b2cd1a3934d
Vic Luo <vicluo96@gmail.com>
2016-11-18 13:00:02 +0800

    Add several scripts to assist development

---
+++ script/run_gnugo_gtp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# for gogui with twogtp which cannot attach parameter for --black
+gnugo --mode gtp
---
+++ script/run_go-ai_default_engine.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# For gogui with twogtp which cannot parse arguments passed to --black
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+${DIR}/../build/go-ai
